dda2936ec5110e5ebf960ed8cf383d1e5bcfb93a
x2Syrupz <86900517+x2Syrupz@users.noreply.github.com>
2022-12-11 10:10:42 +0700

    Update SaveManager.lua

--- addons/SaveManager.lua
+++ addons/SaveManager.lua
@@ -1,7 +1,7 @@
 local httpService = game:GetService('HttpService')
 
 local SaveManager = {} do
-	SaveManager.Folder = 'LinoriaLibSettings'
+	SaveManager.Folder = 'Bone Hub Scripts - Settings'
 	SaveManager.Ignore = {}
 	SaveManager.Parser = {
 		Toggle = {
