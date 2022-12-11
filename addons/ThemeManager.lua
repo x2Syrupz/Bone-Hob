98a43a764d93b039f1385e3f511e08f1c8a840b7
x2Syrupz <86900517+x2Syrupz@users.noreply.github.com>
2022-12-11 10:19:14 +0700

    Update ThemeManager.lua

--- addons/ThemeManager.lua
+++ addons/ThemeManager.lua
@@ -1,6 +1,6 @@
 local httpService = game:GetService('HttpService')
 local ThemeManager = {} do
-	ThemeManager.Folder = 'Bone Hub Scripts - Settings'
+	ThemeManager.Folder = 'LinoriaLibSettings'
 	-- if not isfolder(ThemeManager.Folder) then makefolder(ThemeManager.Folder) end
 
 	ThemeManager.Library = nil
